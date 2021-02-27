d7e8bfabf7bbd206b20b3888b14e0792235862ad
Jeastwin <78631565+Jeastwin@users.noreply.github.com>
2021-02-26 23:06:48 -0600

    commit to master

---
+++ DoriseLab-Vindex.php
@@ -0,0 +1,95 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <title>Julian's Home Page</title>
+    <meta charset="utf-8">
+</head>
+<!--MISM 3320: LAB 4 Dorise 2/14/2021-->
+<style>
+    body {
+        background-color: black;
+        color: white;
+        font-family: Georgia;
+        "Verdana",
+        serif;
+        background-image: url(https://www.publicdomainpictures.net/pictures/190000/velka/usa-city-lights.jpg);
+    }
+
+    h1 {
+        background-color: #FFFFFF;
+        color: #000000;
+        font-family: Ariel, Verdana, sans-serif;
+    }
+
+    h2 {
+        background-color: #CCCCCC;
+        color: #000000;
+        font-family: Ariel, Verdana, sans-serif;
+    }
+
+    .new {
+        color: blue;
+        font-style: italic font-weight:bold;
+    }
+
+    .feature {
+        color: green;
+    }
+
+    <li class="new">My hope is to leverage my education in technology to help bring about change and drive innovation throughout the world.Traveling</li>
+</style>
+
+<head>
+
+<body>
+    <h1 style="text-align:center;">
+        <b>Julian Dorise Web Page</b>
+    </h1>
+    <div><a href="index.html">Home</a> <a href="services.html">Services</a> <a href="contact.html">Contact</a></div>
+    <hr>
+    <p><b><i> My name is Julian Dorise, I am a native Ohioian, who has relocated to Dallas, Texas where I've lived for over 12 years now.
+                I'm currently a college student enrolled at Dallas Baptist University, majoring in Systems Technology and Information Management.
+                This website page is being lauched to introduce myself and shed some insight into some of my interests as we work through this course.</i></b></p>
+
+    <hr>
+
+    <h2>Drive to Educate and Inspire</h2>
+    <p> I'm learning Web Design, as a way to further develop my skills in design principles, HTML, and CSS.</P>
+    <p>
+        <font size="-1" color="darkblue">
+            My hope is to leverage my education in technology to help bring about change and drive innovation throughout the world.
+        </font>
+    </p>
+    <blockquote>
+        "It's not a faith in technology. It's faith in people." - Steve Jobs
+    </blockquote>
+    <h3>What I enjoy</h3>
+    <ol>
+        <li>Reading</li>
+        <li>Design</li>
+        <li>Traveling</li>
+    </ol>
+    <h4>How I'm described</h4>
+    <ul>
+        <li>Compassionate</li>
+        <li>Ambitious</li>
+        <li>Courteous</li>
+        <li>Amiable</li>
+        <li>passionate</li>
+    </ul>
+    <h5>My favorite Websites</h5>
+    <a href="http://forbes,com/innovation.com">Forbes Tech</a>
+    <a href="http://TechCrunch.com"> Tech Crunch Informational</a>
+    <h6>Contact us</h6>
+    <ul>
+        <li class="feature"> Questions?
+            <a href="mailto:jdorise7636@mail.dbu.edu">Email Webmaster</a>
+        </li>
+
+        <a href="http://dbu.edu"><img src="images/Dallas Baptist University.png" width="64" height="64" alt="Dallas Baptist University" /></a>
+        <hr>
+        <div id="footer"><small>Copyright &copy; 2021 Julian Dorise</small></div>
+</body>
+
+</html>
